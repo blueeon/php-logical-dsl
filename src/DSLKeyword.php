869c1ed2b14fb504c8da5d7d8853acc9c2f3dd83
yangke <blueeon@blueeon.net>
2017-05-22 21:12:08 +0800

    add class

---
+++ src/DSLKeyword.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * author:      YangKe <yangke@xiaomi.com>
+ * createTIme:  20170522 19:24
+ * fileName :   DSLKeyword.php
+ */
+class DSLKeyword
+{
+    const KEY_WORDS = [
+        'WHEN',
+        'THEN',
+        //Logical symbol
+        'AND',
+        'OR',
+        'IN',
+        'NOT'
+    ];
+}
